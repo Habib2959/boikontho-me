53d70475454f5ffecbdc839743c7c00f8c114860
unknown <captainamerica.rj@gmail.com>
2022-12-05 11:25:56 +0600

    check clickup

--- src/routes/details/about-us/+page.svelte
+++ src/routes/details/about-us/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import us from '$lib/images/us.png';
-	import ill from '$lib/images/illustration 1.svg';
+	import ill from '$lib/images/Illustration 1.svg';
 </script>
 
 <section class="mt-3 md:mt-6 md:mx-2 lg:mx-4 xl:mx-0 2xl:min-h-[58vh] 2xl:flex 2xl:items-center">
