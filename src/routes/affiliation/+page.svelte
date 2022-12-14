11c266adf6ab0f664215224c4668e1f07a7a1924
unknown <captainamerica.rj@gmail.com>
2022-12-14 14:33:11 +0600

    form making

--- src/routes/affiliation/+page.svelte
+++ src/routes/affiliation/+page.svelte
@@ -1,5 +1,58 @@
 <script>
-	import ill from '$lib/images/our-story.png';
+	import { language } from '../../store';
+	let mobile = '';
+	let address = '';
+	let name = '';
+	let email = '';
+	let gender = '';
+	let dob = '';
+	let platform = [];
+	let link = '';
+	let platformbn = [
+		{
+			label: 'ফেইসবুক গ্রুপ'
+		},
+		{
+			label: 'ফেইসবুক পেইজ'
+		},
+		{
+			label: 'ইন্সটাগ্রাম পেইজ'
+		},
+		{
+			label: 'ইউটিউব চ্যানেল'
+		},
+		{
+			label: 'ওয়েব সাইট/ পোর্টাল'
+		}
+	];
+	let platformEn = [
+		{
+			label: 'ফেইসবুক গ্রুপ'
+		},
+		{
+			label: 'ফেইসবুক পেইজ'
+		},
+		{
+			label: 'ইন্সটাগ্রাম পেইজ'
+		},
+		{
+			label: 'ইউটিউব চ্যানেল'
+		},
+		{
+			label: 'ওয়েব সাইট/ পোর্টাল'
+		}
+	];
+	const handleSubmit = async () => {
+		// try {
+		// 	const data = await bookChoice(name, mobileOrEmail, booklist);
+		// 	successText.set(data.detail);
+		// 	successBtnText.set('Go to home');
+		// 	successbtnLink.set('/');
+		// 	goto('/success');
+		// } catch (err) {
+		// 	alert(err);
+		// }
+	};
 </script>
 
 <svelte:head>
@@ -17,122 +70,121 @@
 <div class="hack-banner" />
 <section class="mt-3 md:mt-6 md:mx-2 lg:mx-4 xl:mx-0 2xl:min-h-[58vh] 2xl:flex 2xl:items-center">
 	<div class="container mx-auto w-[90%] md:w-full">
-		<div>
-			<div class="text-center md:text-left">
-				<h2 class="text-xl font-semibold lg:text-3xl">আমাদের গল্প</h2>
-				<p class="mt-3">
-					মানুষের বন্ধু বই, আর বইয়ের বন্ধু বইকন্ঠ। বইকন্ঠ হলো একটি বহুমাত্রিক বইয়ের সমাধান এবং
-					বাংলাদেশের <span class="text-btnColor">প্রথম অডিওবুক ও ইবুক মার্কেটপ্লেস</span>। বইকে
-					শুধুমাত্র কাগজের পাতায় বন্দি না রেখে অডিও ও ডিজিটাল স্ক্রিনে তুলে ধরছে বইকন্ঠ। এটি যেমন
-					পরিবেশ বান্ধব একই সঙ্গে সাশ্রয়ী যা বইকন্ঠকে একটি কার্বন নিউট্রাল ব্র্যান্ডেও পরিণত করেছে।
-				</p>
-				<p class="mt-3">
-					বইকন্ঠ, ‘<span class="text-btnColor">হ্যাকিউলিস লিমিটেড (Hackules Limited)</span>’ এর একটি
-					সাবসিডিয়ারি ব্র্যান্ড। ২০২১ সালের ২৬ এ আগস্ট বাংলাদেশ সরকার হতে লাইসেন্স প্রাপ্ত হওয়ার পর
-					থেকে দীর্ঘ একবছরের প্রচেষ্টায় আমরা গড়ে তুলেছি বাংলাদেশের সর্বাধুনিক প্রযুক্তির বইয়ের
-					প্ল্যাটফর্মঃ বইকন্ঠ।
-				</p>
-				<p class="mt-3">
-					শুধুমাত্র যানজটের জন্য দেশে প্রতিদিন প্রায় ২ কোটি কর্মঘন্টা নষ্ট হয় এবং জিডিপির ৬-১০%
-					ক্ষতি হয়। <span class="text-btnColor">যানজটে</span> নিজের সময় ও মানসিক স্বাস্থ্য সুরক্ষায়
-					বইকন্ঠের অডিওবুক একটি কার্যকরি সমাধান। এছাড়াও
-					<span class="text-btnColor"> ভ্রমণে, অবসরে বা পরীক্ষার প্রস্তুতিতে</span> অডিওবুকের ভালো চাহিদা
-					রয়েছে।
-				</p>
-				<p class="mt-3">
-					আমাদের মুল লক্ষ্য সর্বাধুনিক ও অভিনব প্রযুক্তির মাধ্যমে বই পঠন ও শ্রবণ কে জনপ্রিয়করণের
-					মাধ্যমে মানুষ ও বইয়ের মধ্যকার ক্রমবর্ধমান দূরত্ব হ্রাস করা এবং বইকে আরও বেশি ​​সহজলভ্য করে
-					তোলা।
-				</p>
-			</div>
+		<div class="text-center max-w-[800px] mx-auto">
+			<h2 class="text-3xl my-3 font-bold text-btnColor">
+				{$language.lang === 'bn' ? 'আপনার পছন্দের বই বইকন্ঠে!' : 'Tempor consequat elit amet anim.'}
+			</h2>
+			<p class="mb-3 text-subtitleColor">
+				{$language.lang === 'bn'
+					? 'আপনি চাইলে আপনার পছন্দের বইয়ের তালিকা জমা দিতে পারেন। আমরা আমাদের পরবর্তি প্রোডাকশনে আপনার পছন্দের বইটিকে অগ্রাধিকার দিয়ে কাজ করব।'
+					: 'Tempor consequat elit amet anim.'}
+			</p>
 		</div>
-		<!-- <div class="mt-6 lg:mt-12">
-			<div class="border-b-2 border-btnColor mb-3 hack-terms-title pb-[1px]">
-				<h1 class="text-lg font-bold lg:text-2xl bg-btnColor text-[#fff] py-1 pr-10 pl-1">
-					Boikontho.com
-				</h1>
-			</div>
-			<div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 md:items-center">
-				<div class="card-with-no-hover text-center">
-					<div>
-						<img
-							src={ill}
-							alt="ill"
-							class="w-[150px] h-[150px] p-2 mx-auto rounded-full border-2 border-btnColor"
-						/>
-					</div>
-					<h3 class="font-semibold text-xl mt-4 lg:text-2xl">One Stop Purchasing Solution</h3>
-					<p class="mt-3">
-						From industrial to grocery and essentials, we have everything you need for your office.
+		<div
+			class="px-3 py-2 bg-white border shadow-sm border-[#e9e7e7] rounded-md max-w-[800px] mx-auto"
+		>
+			<form on:submit|preventDefault={handleSubmit}>
+				<input
+					required
+					type="text"
+					class="mt-2 px-3 py-2 bg-white border shadow-sm border-[#e9e7e7] placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+					placeholder={$language.lang === 'bn' ? 'আপনার নাম' : 'Tempor consequat elit amet anim.'}
+					bind:value={name}
+				/>
+
+				<input
+					required
+					type="text"
+					name="mobile"
+					class="mt-2 px-3 py-2 bg-white border shadow-sm border-[#e9e7e7]  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+					placeholder={$language.lang === 'bn' ? 'মোবাইল' : 'Tempor consequat elit amet anim.'}
+					bind:value={mobile}
+				/>
+				<input
+					required
+					type="email"
+					name="email"
+					class="mt-2 px-3 py-2 bg-white border shadow-sm border-[#e9e7e7]  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+					placeholder={$language.lang === 'bn' ? 'email' : 'Tempor consequat elit amet anim.'}
+					bind:value={email}
+				/>
+				<textarea
+					required
+					class="mt-2 px-3 py-2 bg-white border shadow-sm border-[#e9e7e7] focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+					placeholder={$language.lang === 'bn' ? 'address' : 'address'}
+					bind:value={address}
+				/>
+				<input
+					required
+					type="text"
+					class="mt-2 px-3 py-2 bg-white border shadow-sm border-[#e9e7e7] placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+					placeholder={$language.lang === 'bn'
+						? 'আপনার গ্রুপ, পেইজ, চ্যানেল অথবা ওয়েবসাইটের লিংক'
+						: 'Tempor consequat elit amet anim.'}
+					bind:value={link}
+				/>
+				<div class="mt-2 text-[#9e9e9e]">
+					<p>
+						{$language.lang === 'bn' ? 'date of birth :' : 'Tempor consequat elit amet anim.'}
 					</p>
+					<input
+						type="date"
+						required
+						class="px-3 py-2 bg-white border shadow-sm border-[#e9e7e7] focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+						bind:value={dob}
+					/>
 				</div>
-				<div class="card-with-no-hover text-center">
-					<div>
-						<img
-							src={ill}
-							alt="ill"
-							class="w-[150px] h-[150px] p-2 mx-auto rounded-full border-2 border-btnColor"
-						/>
-					</div>
-					<h3 class="font-semibold text-xl mt-4 lg:text-2xl">One Stop Purchasing Solution</h3>
-					<p class="mt-3">
-						From industrial to grocery and essentials, we have everything you need for your office.
+				<div class="mt-2 text-[#9e9e9e]">
+					<p>
+						{$language.lang === 'bn' ? 'Select your gender :' : 'Tempor consequat elit amet anim.'}
 					</p>
+					<label>
+						<input type="radio" bind:group={gender} value="male" class="accent-[#F43131]" />
+						Male
+					</label>
+					<label>
+						<input type="radio" bind:group={gender} value="female" class="accent-[#F43131]" />
+						Female
+					</label>
 				</div>
-				<div class="card-with-no-hover text-center">
-					<div>
-						<img
-							src={ill}
-							alt="ill"
-							class="w-[150px] h-[150px] p-2 mx-auto rounded-full border-2 border-btnColor"
-						/>
-					</div>
-					<h3 class="font-semibold text-xl mt-4 lg:text-2xl">One Stop Purchasing Solution</h3>
-					<p class="mt-3">
-						From industrial to grocery and essentials, we have everything you need for your office.
+
+				<div class="mt-2 text-[#9e9e9e]">
+					<p>
+						{$language.lang === 'bn' ? 'Select the platform :' : 'Tempor consequat elit amet anim.'}
 					</p>
+					{#if $language.lang === 'bn'}
+						{#each platformbn as platformContent}
+							<label class="block">
+								<input
+									type="checkbox"
+									bind:group={platform}
+									value={platformContent.label}
+									class="accent-[#F43131]"
+								/>
+								{platformContent.label}
+							</label>
+						{/each}
+					{:else}
+						{#each platformEn as platformContent}
+							<label class="block">
+								<input
+									type="checkbox"
+									bind:group={platform}
+									value={platformContent.label}
+									class="accent-[#F43131]"
+								/>
+								{platformContent.label}
+							</label>
+						{/each}
+					{/if}
 				</div>
-				<div class="card-with-no-hover text-center">
-					<div>
-						<img
-							src={ill}
-							alt="ill"
-							class="w-[150px] h-[150px] p-2 mx-auto rounded-full border-2 border-btnColor"
-						/>
-					</div>
-					<h3 class="font-semibold text-xl mt-4 lg:text-2xl">One Stop Purchasing Solution</h3>
-					<p class="mt-3">
-						From industrial to grocery and essentials, we have everything you need for your office.
-					</p>
-				</div>
-				<div class="card-with-no-hover text-center">
-					<div>
-						<img
-							src={ill}
-							alt="ill"
-							class="w-[150px] h-[150px] p-2 mx-auto rounded-full border-2 border-btnColor"
-						/>
-					</div>
-					<h3 class="font-semibold text-xl mt-4 lg:text-2xl">One Stop Purchasing Solution</h3>
-					<p class="mt-3">
-						From industrial to grocery and essentials, we have everything you need for your office.
-					</p>
-				</div>
-				<div class="card-with-no-hover text-center">
-					<div>
-						<img
-							src={ill}
-							alt="ill"
-							class="w-[150px] h-[150px] p-2 mx-auto rounded-full border-2 border-btnColor"
-						/>
-					</div>
-					<h3 class="font-semibold text-xl mt-4 lg:text-2xl">One Stop Purchasing Solution</h3>
-					<p class="mt-3">
-						From industrial to grocery and essentials, we have everything you need for your office.
-					</p>
-				</div>
-			</div>
-		</div> -->
+				<button
+					type="submit"
+					class="mt-3 px-3 py-2 text-[#fff] bg-btnColor border shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+					>{$language.lang === 'bn' ? 'জমা দিন' : 'Submit'}</button
+				>
+			</form>
+		</div>
 	</div>
 </section>
 
@@ -150,7 +202,7 @@
 		background-repeat: no-repeat;
 		margin-top: 60px;
 	}
-	.hack-btn-animation {
+	/* .hack-btn-animation {
 		animation-name: pulse;
 		animation-duration: 2.5s;
 		animation-iteration-count: infinite;
@@ -158,7 +210,7 @@
 		animation-fill-mode: both;
 		animation-play-state: running;
 		display: inline-block;
-	}
+	} */
 	@keyframes pulse {
 		0% {
 			transform: scale(0.95);
